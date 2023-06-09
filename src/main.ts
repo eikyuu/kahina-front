3660866eb7f73f5de740ad8fd1e3b08e83f619fa
eikyuu <v.duguet.dev@gmail.com>
2023-06-09 15:21:29 +0200

    test pour deploy

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
 import localFrExtra from '@angular/common/locales/extra/fr';
 import { appConfig } from './app/app.config';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
 registerLocaleData(localeFr, 'fr-FR', localFrExtra);
 
 bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
