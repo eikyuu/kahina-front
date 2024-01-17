053f9ab45d6bb1436191f23eda2cc78391dbfe7f
vincent <vduguet@apside.fr>
2024-01-17 10:35:57 +0100

    Update login component and navbar

--- src/app/@core/utils/utils.ts
+++ src/app/@core/utils/utils.ts
@@ -20,36 +20,4 @@ export const handleError = (error: HttpErrorResponse) => {
 export const trackByFn = (index: number): number => {
     return index;
 }
-
-
-export class Login {
-
-    _formBuilder = inject(FormBuilder);
-  
-    submitted = false;
-    loading = false;
-  
-    formGroup: { email: any, password: any} = {
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    };
-  
-    loginForm = this._formBuilder.group(this.formGroup);
-  
-    get form() {
-      return this.loginForm.controls;
-    }
-  
-    submit() {
-      this.submitted = true;
-      // stop here if form is invalid
-      console.log(this.loginForm.value, 'this.loginForm.value');
-      if (this.loginForm.invalid) {
-        return;
-      }
-      this.loading = true;
-    }
-  
-    
-  }
   
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,17 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../ui/button/button.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import { Login } from '../../@core/utils/utils';
+import {ReactiveFormsModule, Validators, FormBuilder} from '@angular/forms';
+import { TitleComponent } from '../../ui/title/title.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ButtonComponent, ReactiveFormsModule],
+  imports: [CommonModule, ButtonComponent, ReactiveFormsModule, TitleComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 
 export class LoginComponent {
-  login = new Login();
+  _formBuilder = inject(FormBuilder);
+  
+  submitted = false;
+  loading = false;
+
+  formGroup: { email: any, password: any, remember: any} = {
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    remember: [false],
+  };
+
+  loginForm = this._formBuilder.group(this.formGroup);
+
+  get form() {
+    return this.loginForm.controls;
+  }
+
+  submit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    console.log(this.loginForm.value, 'this.loginForm.value');
+    if (this.loginForm.invalid) {
+      return;
+    }
+    this.loading = true;
+  }
 }
